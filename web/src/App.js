923ede5b66c39ca4a10cf75bbfce624f36f19461
George Paul <george.allen.paul.3@gmail.com>
2017-12-28 16:33:27 -0500

    Add app js

--- web/src/App.js
+++ web/src/App.js
@@ -1,21 +1,68 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import { TextField, List, ListItem, Divider } from 'material-ui'
+import crayola from './resources/crayola.json'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
+const ENTER_KEY = 13
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      colors: [],
+      query: ''
+    }
+  }
+
+  search = () => {
+    const colors = crayola.filter(({ name }) =>  name.toLowerCase().includes(this.state.query.toLowerCase()))
+    this.setState({ colors })
+  }
+
+  handleKeyDown = event => {
+    if (event.keyCode === ENTER_KEY) {
+      event.preventDefault()
+      this.search()
+    }
+  }
+
+  handleQueryChange = (event, query) => {
+    this.setState({ query })
+  }
+
+  renderListItems = () => this.state.colors.map((color, index) => (
+    <div
+      key={`color-${index}`}
+    >
+      <ListItem
+        primaryText={color.name}
+        style={{ backgroundColor: color.hex }}
+      />
+      <Divider />
+    </div>
+  ))
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+      <MuiThemeProvider>
+        <div>
+          <TextField
+            hintText='Search...'
+            floatingLabelFixed
+            fullWidth
+            value={this.state.query}
+            onChange={this.handleQueryChange}
+            onKeyDown={this.handleKeyDown}
+          />
+          <List>
+            {this.renderListItems()}
+          </List>
+        </div>
+      </MuiThemeProvider>
+    )
   }
 }
 
-export default App;
+export default App
